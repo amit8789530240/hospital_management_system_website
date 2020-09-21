bfdd29df842a8318f957c80d3712f21f23120b81
amit kumar <rohit8051824006@gmail.com>
2020-09-21 23:34:26 +0530

    milstone1: made homepage and contact us page

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="angularjs_scripts/angular_script.js"></script>
+</head>
+<input type="button" onclick=>
+<?php
+echo "<input type='text' placeholder='write something'>";
+?>
+<div>amit</div>
+<div >hello</div>
+<div style="display: inline;float: left">world</div>
+<div>kumar</div>
+
+</body>
+</html>
